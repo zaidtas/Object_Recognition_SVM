befa2138cf943e383d638b76cac6b0b3746282a6
zaidtas <zaid.tasneem92@gmail.com>
2019-01-29 20:42:15 -0800

    Added sensor_stick that has the features.py, train_svm.py and capture_features.py

---
+++ sensor_stick/src/sensor_stick/features.py
@@ -0,0 +1,87 @@
+import matplotlib.colors
+import matplotlib.pyplot as plt
+import numpy as np
+from pcl_helper import *
+
+
+def rgb_to_hsv(rgb_list):
+    rgb_normalized = [1.0*rgb_list[0]/255, 1.0*rgb_list[1]/255, 1.0*rgb_list[2]/255]
+    hsv_normalized = matplotlib.colors.rgb_to_hsv([[rgb_normalized]])[0][0]
+    return hsv_normalized
+
+
+def compute_color_histograms(cloud, using_hsv=True):
+
+    #Number of bins
+    nbins=32
+    bins_range=(0,256)
+    # Compute histograms for the clusters
+    point_colors_list = []
+
+    # Step through each point in the point cloud
+    for point in pc2.read_points(cloud, skip_nans=True):
+        rgb_list = float_to_rgb(point[3])
+        if using_hsv:
+            point_colors_list.append(rgb_to_hsv(rgb_list) * 255)
+        else:
+            point_colors_list.append(rgb_list)
+
+    # Populate lists with color values
+    channel_1_vals = []
+    channel_2_vals = []
+    channel_3_vals = []
+
+    for color in point_colors_list:
+        channel_1_vals.append(color[0])
+        channel_2_vals.append(color[1])
+        channel_3_vals.append(color[2])
+
+    # TODO: Compute histograms
+    h_hist = np.histogram(channel_1_vals, bins=nbins, range=bins_range)
+    s_hist = np.histogram(channel_2_vals, bins=nbins, range=bins_range)
+    v_hist = np.histogram(channel_3_vals, bins=nbins, range=bins_range)
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((h_hist[0], s_hist[0], v_hist[0])).astype(np.float64)
+    # Normalize the result
+    normed_features = hist_features / np.sum(hist_features)
+    
+    
+
+    # TODO: Concatenate and normalize the histograms
+
+    # Generate random features for demo mode.  
+    # Replace normed_features with your feature vector
+    #normed_features = np.random.random(96) 
+    return normed_features 
+
+
+def compute_normal_histograms(normal_cloud):
+    norm_x_vals = []
+    norm_y_vals = []
+    norm_z_vals = []
+    #Number of bins
+    nbins=32
+    bins_range=(0,256)
+
+    for norm_component in pc2.read_points(normal_cloud,
+                                          field_names = ('normal_x', 'normal_y', 'normal_z'),
+                                          skip_nans=True):
+        norm_x_vals.append(norm_component[0])
+        norm_y_vals.append(norm_component[1])
+        norm_z_vals.append(norm_component[2])
+
+    # TODO: Compute histograms of normal values (just like with color)
+    x_norm_hist=np.histogram(norm_x_vals,bins=nbins,range=bins_range)
+    y_norm_hist=np.histogram(norm_y_vals,bins=nbins,range=bins_range)
+    z_norm_hist=np.histogram(norm_z_vals,bins=nbins,range=bins_range)
+
+
+    # TODO: Concatenate and normalize the histograms
+    hist_features = np.concatenate((x_norm_hist[0], y_norm_hist[0], z_norm_hist[0])).astype(np.float64)
+    # Normalize the result
+    normed_features = hist_features / np.sum(hist_features)
+    # Generate random features for demo mode.  
+    # Replace normed_features with your feature vector
+    #normed_features = np.random.random(96)
+
+    return normed_features
